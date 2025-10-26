c81e6001b28735ce8fe3d2130cc2860a698eed80
pusapati sathvik <pusapatisathvik@gmail.com>
2025-10-26 17:54:01 +0530

    mm

--- mongodb.sh
+++ mongodb.sh
@@ -43,9 +43,9 @@ VALIDATE $? "ENABLE MONGODB"
 systemctl start mongod &>> $logfile
 VALIDATE $? "START MONGODB"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $logfile
 
-VALIDATE $? "Allowing remote connection to MONGODB" &>> $logfile
+VALIDATE $? "Allowing remote connection to MONGODB" 
 
-systemctl restart mongod
-VALIDATE $? "MONGODB RESTART" &>> $logfile
+systemctl restart mongod &>> $logfile
+VALIDATE $? "MONGODB RESTART" 
