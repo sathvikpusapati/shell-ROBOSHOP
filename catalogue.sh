5e7c16a45b6d5e989556ef83b371e614afe48bf6
pusapati sathvik <pusapatisathvik@gmail.com>
2025-10-26 20:27:34 +0530

    mm

--- catalogue.sh
+++ catalogue.sh
@@ -89,8 +89,16 @@ VALIDATE $? "copying mongo repo file"
 dnf install mongodb-mongosh -y &>> $logfile
 VALIDATE $? "installing mongodb client"
 
-mongosh --host $MONGODB_IP </app/db/master-data.js &>> $logfile
-VALIDATE $? "loading master data into mongodb"
+
+INDEX=$(mongosh mongodb.thanunenu.space --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+
+    mongosh --host $MONGODB_IP </app/db/master-data.js &>> $logfile
+    VALIDATE $? "loading master data into mongodb"
+else
+
+    echo -e " ALRAEDY DATA LOADED INTO DATA BASE $Y SKIPPING $N"
+fi
 
 systemctl restart catalogue  &>> $logfile
 VALIDATE $? "restarting catalogue"
